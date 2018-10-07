e48c0d3a6c077dc69a8441ee7848dfd12efce4a5
torchiba <account@torch-design.work>
2018-10-07 13:26:22 +0900

    テンプレート少しさわりつつビルドまで進める

---
+++ components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+	<nav class="header">
+		<ul>
+			<li><nuxt-link to="/">ホーム</nuxt-link></li>
+			<li><nuxt-link to="/blog">ブログ</nuxt-link></li>
+			<li><nuxt-link to="/profile">プロフィール</nuxt-link></li>
+			<li><nuxt-link to="/contact">お問い合わせ</nuxt-link></li>
+		</ul>
+	</nav>
+</template>
+
+
+<style lang="scss" scoped>
+.header {
+	ul {
+		display: flex;
+	}
+	a {
+		text-decoration: none;
+		&:hover,
+		&.nuxt-link exact-active {
+			text-decoration: underline;
+		}
+	}
+}
+</style>
+
